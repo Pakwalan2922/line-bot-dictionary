f3502bd6506684f01152225b830be3d17bd4ea43
June Muen <junnies.junny@gmail.com>
2024-06-20 07:01:23 +0700

    edit file

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ function handleEvent(event) {
 }
 
 // listen on port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+  //console.log(`listening on ${port}`);
+  console.log('Server started on port 5000');
 });
